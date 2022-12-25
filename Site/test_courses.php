113caed2c472bbb4e6075fde45c50a1c25f3f7aa
safaamigo <safaabatrahi7@gmail.com>
2022-12-25 01:45:36 +0100

    change31

--- Site/test_courses.php
+++ Site/test_courses.php
@@ -15,83 +15,28 @@
   include("navbar.php");
   ?>
 
-      <!-- *************************************************************************** -->
-      <div class="container row">
-      <div class="col-lg-4" id="search-container">
-      <input
-          type="search" id="search-input" placeholder="Search.."/>
-        <button id="search" >Search</button>
-        <?php
-                if(isset($_SESSION['admin'])){
-                    ?>
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Open modal for @getbootstrap</button>
-
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form>
-          <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">Recipient:</label>
-            <input type="text" class="form-control" id="recipient-name">
-          </div>
-          <div class="mb-3">
-            <label for="message-text" class="col-form-label">Message:</label>
-            <textarea class="form-control" id="message-text"></textarea>
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Send message</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<?php
-                }
-              ?>
-        <ul class="buttons">
-          <li class="button-value" onclick="filterProduct('all')"> ALL</li>
-          <li class="button-value" onclick="filterProduct('JS')">JS</li>
-          <li class="button-value" onclick="filterProduct('PHP')">PHP</li>
-          <li class="button-value" onclick="filterProduct('CSS')">CSS</li>
-          <li class="button-value" onclick="filterProduct('HTML')">HTML</li>
-        </ul>
-      </div>
-        <div class="col-lg-8" id="products">
-              <!-- // $req="SELECT * FROM cours";
-              // $res=mysqli_query($con,$req);
-              
-              // while($array=mysqli_fetch_assoc($res)){
-              //   print_r($array);
-              // }
-              // for($i=0;count($array);$i++){
-              //   print_r($array[$i]);
-              // }
-            //   while($row = mysqli_fetch_assoc($res)){
-            //     $array[] = $row;
-            //   }
-            //   foreach ($array as $key => $value) {
-            //     // $arr[3] sera mis à jour avec chaque valeur de $arr...
-            //     echo "{$key} => {$value} ";
-            //     var_dump($arr);
-            // } -->
-          
-          
+        <div class="container m-auto w-100">
+            <div class="row">
+                <div class="col-lg-3 col-md-5 col-sm-4 " >
+                        <input type="text" placeholder="Search...">
+                        <ul class="buttons">
+                                <li class="button-value" onclick="filterProduct('all')"> ALL</li>
+                                <li class="button-value" onclick="filterProduct('JS')">JS</li>
+                                <li class="button-value" onclick="filterProduct('PHP')">PHP</li>
+                                <li class="button-value" onclick="filterProduct('CSS')">CSS</li>
+                                <li class="button-value" onclick="filterProduct('HTML')">HTML</li>
+                        </ul>
+                        <input type="range" min="0" max="100" value="0" oninput="document.getElementById('AfficheRange').textContent=value" />
+                        <span id="AfficheRange">0</span>
+                </div>
+                <div class="row col-9 d-flex text-center justify-content-evenly " id="products"></div>
+            </div>
         </div>
-    </div>
-
-  <?php
+        <?php
     include("footer.php");
 
   ?> 
-  <script>
+        <script>
 
 let products = {
     data: [
@@ -112,6 +57,9 @@ $res = mysqli_query($con, $req);
   for (let i of products.data) {
     //Create Card
     let card = document.createElement("div");
+    card.classList.add("col-md-3");
+    card.classList.add("col-sm-2");
+
     //Card should have category and should stay hidden initially
     card.classList.add("card", i.category, "hide");
     //image div
