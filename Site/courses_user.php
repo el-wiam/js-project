a430def96fdbddafe6f42af230b1fc84a603d95b
safaamigo <safaabatrahi7@gmail.com>
2022-12-30 21:54:21 +0100

    change116

--- Site/courses_user.php
+++ Site/courses_user.php
@@ -5,27 +5,8 @@
       include("../header_footer/navbar.php");
       ?>
       <!-- *************************************************************************** -->
-<<<<<<< HEAD
-<<<<<<< HEAD
       <div class="container row ">
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f07dae9f109b760c37231ca22e1821707212c910
-      <div class="container row tout-container">
-
-      <div class="container row ">
-<<<<<<< HEAD
->>>>>>> 2db2241fe3c5566db0bb766c73dcdc0914cb9f34
->>>>>>> 743898bf9c949c7171b4052fa84153ecdbc84d9c
-=======
-
->>>>>>> f07dae9f109b760c37231ca22e1821707212c910
       <div class="col-lg-4 " id="search-container">
-
-      <div class="container row">
-      <div class="col-lg-3 " id="search-container">
           <input type="search" id="search-input" placeholder="Search.."/>
           <div class="items-head">
             <p>Categories</p>
@@ -36,7 +17,6 @@
           <li class="items-content" onclick="filterProduct('JS')"><span>JS</span> <i class="fa fa-angle-right"></i></li>
           <li class="items-content" onclick="filterProduct('PHP')"><span>PHP</span> <i class="fa fa-angle-right"></i></li>
           <li class="items-content" onclick="filterProduct('CSS')"><span>CSS</span> <i class="fa fa-angle-right"></i></li>
-          <li class="items-content" onclick="filterProduct('HTML')"><span>HTML</span> <i class="fa fa-angle-right"></i></li>
           <li class="items-content" onclick="filterProduct('HTML')"><span>HTML</span> <i class="fa fa-angle-right"></i></li>
         </ul>
         <?php
