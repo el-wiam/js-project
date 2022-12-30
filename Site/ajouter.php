820d41647f329e7866194957137104f5792efbb6
safaamigo <safaabatrahi7@gmail.com>
2022-12-30 15:16:52 +0100

    chnage829

--- Site/ajouter.php
+++ Site/ajouter.php
@@ -9,13 +9,15 @@ if(isset($_POST['submit'])){
     move_uploaded_file($_FILES['img']['tmp_name'],$upload);
     // $_FILES['userfile']['tmp_name'] Le nom de fichier temporaire du fichier dans lequel le fichier téléchargé a été stocké sur le serveur.
 
-    $req="INSERT INTO cours VALUES (NULL,'$categories_select','$description',$price,'$img')";
+    $req="INSERT INTO cours VALUES (NULL,'$categories_select','$description',$price,'$upload')";
 
     $res=mysqli_query($con,$req);
 
     if($res ){
         // if(!empty($img) && !empty($description) && !empty($categories_select) && !empty($price))
-        header("location:ajouter_cours.php");
+        // header("location:ajouter_cours.php");
+    echo $upload;
+
         // $_SESSION['status']='inserted succesful';
     }
     else{
--- Site/home_user.php
+++ Site/home_user.php
@@ -1,8 +1,9 @@
 <?php
+
     session_start();
     if(isset($_SESSION['user'])){
-        
-  include("../header_footer/navbar.php");
+        include("../connexion.php");
+        include("../header_footer/navbar.php");
   ?>
 
       <div class="card text-center test">
@@ -19,94 +20,90 @@
         <h1 class="text-center"> <span id="slash">/</span> Courses</h1>
     </div>
 
-
     <div class="container swiper mb-5" id="about">
-        <div class="slide-content">
-            <div class="card-wrapper swiper-wrapper">
-                <div class="swiper-slide">
-                    <div class="image-content">
-                        <span class="overlay"></span>
-
-                        <div class="card-image">
-                            <img src="images/laptop.png" alt="" class="card-img">
-                        </div>
-                    </div>
-
-                    <div class="card-content">
-                        <!-- <h2 class="name">David Dell</h2> -->
-                        <p class="description">HTML Attributes And Values</p>
-
-                        <span class="span-font">$19.9</span>
-                    </div>
-                </div>
-
-                <div class="swiper-slide">
-                    <div class="image-content">
-                        <span class="overlay"></span>
-
-                        <div class="card-image">
-                            <img src="images/img-1.jpg" alt="" class="card-img">
-                        </div>
-                    </div>
-
-                    <div class="card-content">
-                        <!-- <h2 class="name">David Dell</h2> -->
-                        <p class="description">Manipulating Crud Using Php</p>
-
-                        <span class="span-font">$45.9</span>
-                    </div>
-                </div>
-
-                <div class="swiper-slide">
-                    <div class="image-content">
-                        <span class="overlay"></span>
-
-                        <div class="card-image">
-                            <img src="images/img-3.jpg" alt="" class="card-img">
-                        </div>
-                    </div>
-
-                    <div class="card-content">
-                        <!-- <h2 class="name">David Dell</h2> -->
-                        <p class="description">CSS Selectors</p>
-
-                        <span class="span-font">$69.9</span>
-                    </div>
-                </div>
-
-                <div class="swiper-slide">
-                    <div class="image-content">
-                        <span class="overlay"></span>
-
-                        <div class="card-image">
-                            <img src="images/img-4.jpg" alt="" class="card-img">
-                        </div>
-                    </div>
-
-                    <div class="card-content">
-                        <!-- <h2 class="name">David Dell</h2> -->
-                        <p class="description">JS Coding DOM</p>
-
-                        <span class="span-font">$45.9</span>
-                    </div>
-                </div>
-               
-            </div>
-            </div>
-            <div class="btn-style-all">
-                <a href="courses_user.php" type="button" class="btn ">all courses</a>
-
-            </div>
+        <!-- <div id="test"> </div> -->
+        <div class="btn-style-all">
+            <a href="courses_user.php" type="button" class="btn ">all courses</a>
         </div>
-
-
-        <div class="swiper-button-next swiper-navBtn"></div>
-        <div class="swiper-button-prev swiper-navBtn"></div>
-        <!-- <div class="swiper-pagination"></div> -->
     </div>
-
-
+    <script>
+        let products = {
+    data: [
       <?php
+        $req=mysqli_query($con,"SELECT * FROM cours");
+        while($test = mysqli_fetch_assoc($req)) {
+            echo ("{
+              productName: \"" . $test['description'] . "\",
+              category: \"" . strtoupper($test['categorie']) . "\",
+              price: \"" . $test['price'] . "\",
+              image: \"" . $test['img'] . "\",
+            },");
+          }
+            ?>
+    ],
+};
+for (let i of products.data) {
+    //console.log(i.productName)
+    //Create Card
+    let slide = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    slide.classList.add("slide-content", i.category);
+
+    let card_wrapper=document.createElement("div");
+    card_wrapper.classList.add("card-wrapper","swiper-wrapper");
+
+    let swiper_slide = document.createElement("div");
+    swiper_slide.classList.add("swiper-slide");
+
+    let image_content=document.createElement("div");
+    image_content.classList.add("image-content");
+
+    let overlay=document.createElement("span");
+    overlay.classList.add("overlay");
+
+    let card_img=document.createElement("div");
+    card_img.classList.add("card-image");
+
+    let img=document.createElement("img");
+    img.classList.add("card-img")
+    img.setAttribute("src",i.image);
+
+    let card_content=document.createElement("div");
+    card_content.classList.add("card-content");
+
+    let description=document.createElement("p");
+    description.classList.add("description");
+    description.innerText = i.productName;
+
+    let span=document.createElement("span");
+    span.classList.add("span-font");
+    span.innerText = i.price;
+
+
+    card_content.appendChild(span);
+    card_content.appendChild(description);
+
+    card_img.appendChild(img);
+
+    image_content.appendChild(card_img);
+    image_content.appendChild(overlay);
+
+    swiper_slide.appendChild(image_content);
+    swiper_slide.appendChild(card_content);
+
+    card_wrapper.appendChild(swiper_slide)
+
+    slide.appendChild(card_wrapper);
+
+    document.getElementById("about").appendChild(slide);
+
+  }
+console.log( document.getElementById("about"));
+    
+</script>
+
+
+<?php
         include("../header_footer/footer.php");
  } 
  else{
