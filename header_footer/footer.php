b0025b76bf482c8379bdcbc01fff243d6c80c815
safaamigo <safaabatrahi7@gmail.com>
2022-12-31 01:36:40 +0100

    change122

--- header_footer/footer.php
+++ header_footer/footer.php
@@ -1,5 +1,5 @@
 
-<footer style="text-align: center"> Copyright &copy; <script>document.write(new Date().getFullYear())</script> SAFAA BATRAHI & WIAM EL FARD</footer>
+<footer style="text-align: center" class="mt-5"> Copyright &copy; <script>document.write(new Date().getFullYear())</script> SAFAA BATRAHI & WIAM EL FARD</footer>
 <script src="../bootstrap/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>  
 
 </body>
