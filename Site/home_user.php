9e4e4e530ee67519945573ee2b0cceb6d15cae5c
safaamigo <safaabatrahi7@gmail.com>
2023-01-01 17:54:32 +0100

    change220

--- Site/home_user.php
+++ Site/home_user.php
@@ -26,6 +26,48 @@
         <h1 class="text-center"> <span id="slash">/</span> Courses</h1>
     </div>
 
+
+   
+
+<!-- // $req=mysqli_query($con,"SELECT * FROM cours");
+// $arr=[];
+// while($test = mysqli_fetch_assoc($req)){
+//   array_push($arr,$test);
+// };
+// // $i=0;
+// for($i=0;$i<3;$i++){
+
+//   echo "<pre>";
+//           print_r(array_rand($arr[i]['description']),3);
+//         echo "</pre>";
+// }
+
+//       // foreach (array_rand($arr,3) as $value => $val) {
+//       //   // foreach($val as $v){
+//       //   echo "$val <br>";
+//       //   } -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <div class="slider_container swiper mb-5" >
     <div class="swiper mySwiper">
       <div class="swiper-wrapper">
